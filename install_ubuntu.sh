2ba2596b061b6d3d8ef8947a0f1858080e0971a9
Axel <axelfernandezcurros@gmail.com>
2022-03-14 22:15:52 +0100

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/env bash
 # To install Ubuntu in Termux Android App
 pkg update -y
-package_list=("iproute2" "proot" "wget" "git" "proot-distro" "bash" "bzip2" "coreutils" "curl" "findutils" "gzip" "dnsutils" "ncurses-utils" "sed" "tar" "xz-utils")
+package_list=("iproute2" "proot" "wget" "git" "tsu" "proot-distro" "bash" "bzip2" "coreutils" "curl" "findutils" "gzip" "dnsutils" "ncurses-utils" "sed" "tar" "xz-utils")
 for package in "${package_list[@]}"; do
   if ! which "${package}"; then
     pkg install -y "${package}"
