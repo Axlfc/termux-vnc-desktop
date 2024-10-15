d20cad017055ce616c0f6610df7d659a9d254418
Axel <axelfernandezcurros@gmail.com>
2024-10-15 22:38:38 +0200

    Update finish_install.sh

--- finish_install.sh
+++ finish_install.sh
@@ -25,17 +25,3 @@ mate-session
   echo "${xstartup_text}" > /data/data/com.termux/files/home/.vnc/xstartup
   sudo chmod +x /data/data/com.termux/files/home/.vnc/xstartup
 fi
-exit
-
-#rm "/data/data/com.termux/files/home/quisoc"
-apt-get upgrade -y
-
-root_customizer_package_list=("nemo" "git" "gitk" "python3" "guake" "tmux" "okular" "vlc" "firefox" "sysmontask" "wireshark" "thunderbird" "pluma" "gpaint" "clonezilla" "screenshots" "gparted" "grsync" "shotwell" "fonts_firacode" "fonts-hack" "fonts-hermit" "fonts-roboto" "inkscape" "gimp" "libreoffice")
-for package in "${root_customizer_package_list[@]}"; do
-  sudo customizer-install -v -o -k "${package}"
-done
-
-user_customizer_package_list=("prompt" "gitprompt" "history_optimization" "terminal_background" "fastcommands" "bashfunctions" "gitbashfunctions" "shortcuts" "changebg" "bashcolors" "cheat" "converters" "emojis" "templates" "network" "discord" "duckduckgo" "gitcm" "wikipedia" "fonts-lato" "fonts-alegreya_sans" "fonts-noto_sans" "fonts-oswald" "fonts-oxygen" "system_fonts" "telegram")
-for package in "${user_customizer_package_list[@]}"; do
-  customizer-install -v -o "${package}"
-done
