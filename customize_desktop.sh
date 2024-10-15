036e531180b48607c3a657cfffdaf31f568bf01c
Axel <axelfernandezcurros@gmail.com>
2024-10-15 23:26:00 +0200

    Update customize_desktop.sh

--- customize_desktop.sh
+++ customize_desktop.sh
@@ -1,7 +1,9 @@
 #rm "/data/data/com.termux/files/home/quisoc"
 sudo apt-get upgrade -y
 
-root_customizer_package_list=("nemo" "git" "gitk" "python3" "guake" "tmux" "okular" "vlc" "firefox" "sysmontask" "wireshark" "thunderbird" "pluma" "gpaint" "clonezilla" "screenshots" "gparted" "grsync" "shotwell" "fonts_firacode" "fonts-hack" "fonts-hermit" "fonts-roboto" "inkscape" "gimp" "libreoffice")
+#root_customizer_package_list=("nemo" "git" "gitk" "python3" "guake" "tmux" "okular" "vlc" "firefox" "sysmontask" "wireshark" "thunderbird" "pluma" "gpaint" "clonezilla" "screenshots" "gparted" "grsync" "shotwell" "fonts_firacode" "fonts-hack" "fonts-hermit" "fonts-roboto" "inkscape" "gimp" "libreoffice")
+
+root_customizer_package_list=("nemo" "git" "gitk" "python3" "guake" "tmux" "okular" "vlc" "firefox" "sysmontask" "wireshark" "thunderbird" "pluma" "gpaint" "clonezilla" "screenshots" "gparted" "grsync" "shotwell" "systemFonts" "inkscape" "gimp" "libreoffice")
 for package in "${root_customizer_package_list[@]}"; do
   sudo customizer-install -v -o -k "${package}"
 done
